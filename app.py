67fb0f871a4015a02c943072d2539c25fb4b5493
cuateverde <ikermarcor5@gmail.com>
2022-04-21 10:35:01 -0700

    correcion de paquetes y sintaxis en app.py

--- app.py
+++ app.py
@@ -1,26 +1,22 @@
+from doctest import run_docstring_examples
 from urllib.request import Request
 from flask import Flask, render_template,request,flash,redirect,url_for
-from waitress import serve
-from sympy import true
 
 app = Flask(__name__)
 app.secret_key = b'1293hda7821hgdn{q'
 
-@app.route("/Sign-in", methods=['GET','POST'])
+@app.route("/", methods=['GET','POST'])
+def index():
+    if request.method == 'GET':
+        return render_template('Base.html')
+
+@app.route("/sign-in", methods=['GET','POST'])
 def Sign():
     if request.method == 'GET':
         return render_template('Sign-in.html')
     if request.method == 'POSt':
-        value = Request.form['Sign-in']
-        if value == 'submit':
+        value = Request.form['sign-in']
+        #if value == 'submit':
             
-
-@app.route("/", methods=['GET','POST'])
-def index():
-    if request.method == 'GET':
-       
-        return render_template('Base.html')
-    
-
 if __name__ == "__main__":
-    serve(app, host="192.168.1.20", port=5000)
+    app.run(debug=True)
